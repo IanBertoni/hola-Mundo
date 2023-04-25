c87e8103ce668f7f847c7c7552d06c3cf1494cfc
IanBertoni <ianbertoni@hotmail.com>
2023-04-25 19:01:49 -0300

    a

---
+++ Typescript deep dive/codigo.js
@@ -0,0 +1,97 @@
+/*
+let string = "";
+let number = 1;
+let booleano = true/false;
+
+const = "markinhos" // se tiene que declarar e inicializar al mismo tiempo
+let permite declarar e inicializarla por separado
+undefined: La variable existe pero no esta inicializada
+null: Algo no se encuentra disponible
+
+let nombre = prompt("Decime tu nombre: ") la respuesta del usuario se guarda en la variable "nombre"
+
+let numero = 10; 
+numero += 5; es igual a sumar numero=numero + 5
+
+let numero = 10; 
+numero -= 5; es igual a restar numero=numero - 5
+
+antes del signo "=" si pones un signo "*" por ejemplo, multiplicas el numero por el numero que le indiques
+
+let saludo = "Hola markitos ";
+let pregunta ="Como estas?";
+
+let frase = saludo + pregunta;
+
+document.write(frase);
+-------------------------------------------------------------------------
+nombre = "Ian Bertoni"
+Frase = `soy ${nombre} y estoy practicando`
+
+document.write(Frase)
+
+
+let numero = 23;
+let numero2 = 13;
+let texto = "23";
+let texto2 = "texto 2";
+
+
+document.write(numero == numero2); False
+document.write(numero != numero2); Verdadero
+document.write(texto != texto2); Verdadero
+document.write(numero == texto); Verdadero ya que no analiza el tipo de dato sino el valor del mismo
+document.write(numero === texto); False el triple "=" se compara si son estrictamente igual
+
+let valor = true;
+let valor2 = true;
+
+let resultado = valor && valor2; TRUE
+
+let resultado2 = valor || valor2; TRUE
+
+let resultado3 = !valor; FALSE
+
+document.write(resultado, resultado2, resultado3)
+
+----------------------------------  Condicionales  -----------------
+
+
+
+if (condicion){
+    alert("hola")
+} 
+
+ Un ejemplo: 
+ if (10 < 15){
+    alert("10 es menor que 15")
+ }
+ 
+  if (15 < 10){ 
+    alert()
+ }
+ En este caso no se ejecuta por que la condicion no es verdadera
+ 
+
+let nombre = "Marcos";
+
+if (nombre == "Ian"){
+   alert(`tu nombre es ${nombre}`)  Si esta condicion no se cumple pasa a la de abajo
+}
+
+else if (nombre == "Bertoni"){
+   alert(`tu apellido es ${nombre}`)   se puede usar tantos else if como quieras pero tan solo un if y un else
+}
+else {
+   alert("tu nombre es otro amigo") El else se usa como condicion final de salida.
+}
+*/
+
+
+
+
+
+
+
+ 
+ 
