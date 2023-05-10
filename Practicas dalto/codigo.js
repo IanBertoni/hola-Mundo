b1b084a02f00277230945b7df228dbea4d19fcc8
IanBertoni <ianbertoni@hotmail.com>
2023-05-10 06:05:55 -0300

    Update

--- Practicas dalto/codigo.js
+++ Practicas dalto/codigo.js
@@ -54,7 +54,7 @@ let resultado3 = !valor; FALSE
 
 document.write(resultado, resultado2, resultado3)
 
-----------------------------------  Condicionales  -----------------
+----------------------------------  Condicionales  ---------------------------------------------------------------------------------------------
 
 
 
@@ -242,4 +242,168 @@ for (let array in array2) {
   }
 }
 
+
+--------------------------------------------------------------------Funciones------------------------------------------------
+
+
+let num1 = 32;
+let num2 = 12;
+
+let res = num1 + num2;
+
+function suma (a, b){
+    let resultado = a + b;
+    return resultado;
+}
+
+suma(2,4);
+
+
+const saludar = function(nombre){
+    let frase = `hola ${nombre}, como andas?`;
+    document.write(frase);
+}
+
+// Funcion flecha, primero se define el nombre de la funcion, luego los valores que acepte, y por ultimo la flecha que reemplaza a "Function"
+const saludar = (nombre) => { 
+    let frase = `hola ${nombre}, como andas?`;
+    document.write(frase);
+}
+
+
+
+saludar("Marcos")
+
+
+
+
+class animal {
+  constructor(especie, nombre, edad, color, sexo) {
+    // Entreparentesis van los parametros
+    this.especie = especie; // this es con lo que creo la propiedad.
+    this.edad = edad; //constructor(edad) != this.edad porque el primero es un parametro, y la segunda es una propiedad.
+    this.color = color; // da igual como le pongas de nombre despues del punto(.), es solo para darle nombre a la propiedad.
+    this.sexo = sexo; // con el this lo que hacemos es seleccionar el objeto y le decimos que va a ser igual a sexo por ejemplo.
+    this.nombre = nombre;
+    this.information = `hola me llamo ${this.nombre} y soy un ${this.especie}, tengo ${this.edad} años de edad, mi pelaje es de color ${this.color} y por si te lo preguntabas soy ${this.sexo}.`;
+  } // el this solo se usa dentro de los objetos y solo para el constructor.
+  verInfo() { 
+    document.write(this.information + "<br>");
+  } // las funciones que creamos adentro de un objeto, se llaman metodos, y este metodo muestra la informacion de "X" animal.
+}
+
+//ahora creemos un animal.
+// cuando trabajamos en objetos es recomendable usar const en vez de let o var.
+
+const perro = new animal("perro", "Pirata", 5, "Blanco y dorado", "macho");
+
+const gato = new animal("gato", "Boots", 23, "Amarillo y marron", "hembra");
+
+const leon = new animal("leon", "Leonidas", 293, "verde", "macho");
+
+
+// console.log(perro.information);
+// console.log(gato.information);
+// console.log(leon.information);
+
+// los console.log se puede reemplazar haciendo un metodo.
+
+perro.verInfo();
+gato.verInfo();
+leon.verInfo();
+
+// de esta manera se usa un metodo para pedir la informacion de los animales 
+
+
+
+//--------Caracteristicas de la programacion orientada a objetos: ----------------------------------------------------------
+
+    Abstracción: simplifica conceptos complejos:
+   Es simplificar las cosas complicadas al centrarnos en lo más importante y dejar de lado los detalles innecesarios. Es como ver el panorama general en lugar de todos los pequeños detalles.
+   ------------------------------------------------------------------------------------------------------------------------ 
+   Modularidad: divide un sistema en partes más pequeñas y manejables: Es dividir un sistema en partes más pequeñas y fáciles de manejar. Es como construir un rompecabezas, donde cada pieza tiene una función específica y se puede trabajar en ellas por separado.
+   ------------------------------------------------------------------------------------------------------------------------
+    Encapsulamiento: oculta los detalles internos y expone una interfaz controlada:
+    Es esconder los detalles internos y mostrar solo la parte que necesitamos. Es como tener una caja de herramientas donde solo ves las herramientas que puedes usar, pero no necesariamente cómo funcionan por dentro.
+   ------------------------------------------------------------------------------------------------------------------------
+   Polimorfismo: permite que los objetos tengan diferentes comportamientos sus propiedades:
+    Es la capacidad de los objetos para comportarse de diferentes maneras según el contexto. Es como tener un objeto que puede hacer diferentes tareas dependiendo de lo que se le pida, como un actor que puede interpretar diferentes roles en diferentes películas.
+
+------------Otros conceptos de la programacion orientada a objetos:   --------------------------------------------
+
+
+   Herencia: Es como cuando un objeto especializado hereda características de otro objeto más general. Por ejemplo, un "Perro" hereda características de un "Animal". Así, podemos reutilizar y ampliar el código existente.
+
+    Métodos estáticos: Son funciones especiales que puedes usar sin necesidad de crear un objeto. Son útiles para hacer operaciones que no dependen de un objeto en particular. Puedes pensar en ellos como acciones independientes. Por ejemplo, una "Calculadora" puede tener un método estático "sumar" que puedes usar directamente sin crear una calculadora.
+
+    Métodos de acceso (Getters, Setters): Son formas de obtener y modificar información de un objeto. Los getters te permiten obtener el valor de algo, como obtener el nombre de una persona. Los setters te permiten cambiar o asignar un nuevo valor, como cambiar el nombre de una persona. Son útiles para mantener el control y la seguridad de los datos de un objeto.
+
+
+class Animal {
+  constructor(especie, nombre, edad, color, sexo) {
+    this.especie = especie;
+    this.edad = edad;
+    this.color = color;
+    this.sexo = sexo;
+    this.nombre = nombre;
+    this.information = `Hola, me llamo ${this.nombre} y soy un ${this.especie}. Tengo ${this.edad} años de edad, mi pelaje es de color ${this.color} y soy ${this.sexo}.`;
+  }
+
+  verInfo() {
+    document.write(this.information + "<br>");
+  }
+  ladrar() { //prueba de polimorfismo junto a la llamada del metodo de abajo (.ladrar)*
+    if (this.especie == "perro") {
+      document.write(`woof <br>`);
+    } else {
+      document.write(`no puede ladrar porque es un ${this.especie} <br>`);
+    }
+perro.ladrar(); //*
+gato.ladrar(); //*
+leon.ladrar(); //*Estos 3 metodos no van adentro de la funcion, los acomode aca para poder seguir practicando sin la molestia del codigo comentado
+  }
+}
+
+const perro = new Animal("perro", "Pirata", 5, "Blanco y dorado", "macho");
+const gato = new Animal("gato", "Boots", 23, "Amarillo y marron", "hembra");
+const leon = new Animal("leon", "Leonidas", 293, "verde", "macho");
+
+
+perro.verInfo();
+gato.verInfo();
+leon.verInfo();
+
+
 */
+
+class Animal {
+  constructor(especie, nombre, edad, color, sexo) {
+    this.especie = especie;
+    this.edad = edad;
+    this.color = color;
+    this.sexo = sexo;
+    this.nombre = nombre;
+    this.information = `Hola, me llamo ${this.nombre} y soy un ${this.especie}. Tengo ${this.edad} años de edad, mi pelaje es de color ${this.color} y soy ${this.sexo}.`;
+  }
+
+  verInfo() {
+    document.write(this.information + "<br>");
+  }
+}
+
+class Perro extends Animal {
+  constructor(especie, edad, color, raza) {
+    super(especie, edad);
+    this.raza = raza;
+  }
+  ladrar() {
+    alert("Woof");
+  }
+}
+//ACLARACION IMPORTANTE: Un objeto y una clase NO pueden tener el mismo nombre(por eso la clase Perro tiene la "P" mayuscula)
+const perro = new Perro("perro", "Pirata", 5, "Blanco y dorado", "macho");
+const gato = new Animal("gato", "Boots", 23, "Amarillo y marron", "hembra");
+const leon = new Animal("leon", "Leonidas", 293, "verde", "macho");
+
+perro.ladrar(); // "Woof"
+gato.ladrar(); // gato.ladrar() its not a function.
